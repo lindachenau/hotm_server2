ece9e7d006ac7dd706d2417c5236ec2a740d45b8
Linda Chen <lindachenau@gmail.com>
2020-03-18 08:20:07 +1100

    Connecting redis cloud is working on localhost.

--- reminder.js
+++ reminder.js
@@ -1,66 +1,6 @@
-require('dotenv').config({path: __dirname + '/.env'})
-const nodemailer = require("nodemailer");
 const moment = require("moment");
-
-const express = require('express')
-const redis = require('kue/node_modules/redis');
-const url = require('url');
-const kue = require('kue');
-
-const app = express();
-app.redisClient = (function() {
-  const redisUrl = url.parse(process.env['REDISCLOUD_URL']);
-  const client = redis.createClient(redisUrl.port, redisUrl.hostname);
-  if (redisUrl.auth) {
-    client.auth(redisUrl.auth.split(":")[1]);
-  }
-  return client;
-})()
-
-app.redisClient.on('connect', function () {
-  console.info('successful connection to redis server');
-});
-
-app.redisClient.on('error', function (err) {
-  console.log('Redis error encountered', err);
-});
-
-app.redisClient.on('end', function() {
-  console.log('Redis connection closed');
-});
-
-const queue = kue.createQueue({
-  redis: {
-      createClientFactory: function() {
-          return app.redisClient;
-      }
-  }
-});
-
-/*
-  Here we are configuring our SMTP Server details.
-  STMP is mail server which is responsible for sending and receiving email.
-*/
-const smtpTransport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env['GMAIL_USER'],
-    pass: process.env['GMAIL_PASS']
-  }
-});
-
-const scheduleJob = data => {
-  queue.createJob(data.jobName, data.params)
-    .attempts(3)
-    .delay(data.time - Date.now()) // relative to now.
-    .save();
-};
-
-queue.process("sendReminder", async function(job, done) {
-  let { data } = job;
-  await smtpTransport.sendMail(data);
-  done();
-});
+const kue = require("./kue");
+require("./worker");
 
 const set = (req) => {
   return new Promise ((resolve) => {
@@ -87,7 +27,7 @@ const set = (req) => {
           html : `<h3>Your appointment with Hair On The Move 2 U on ${timeText} is coming in 2 days. We look forward to serving you!</h3>`
         }
       };
-      scheduleJob(args);
+      kue.scheduleJob(args);
       
       resolve(`Reminder for appointment on ${timeText} will be sent to ${email}.`)
     }
